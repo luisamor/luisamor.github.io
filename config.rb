05466e1f2ddc3c5ed29189a2a6e0f0c071c136af
Luis Amor <luis.amor.alvarez@gmail.com>
2017-09-30 23:29:32 +0200

    fix developer gem version

--- config.rb
+++ config.rb
@@ -40,10 +40,6 @@ page '/*.txt', layout: false
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
  
 
 # Assets
@@ -63,4 +59,13 @@ configure :build do
 
   # Enable cache buster
   activate :asset_hash
+end
+
+activate :deploy do |deploy|
+  deploy.deploy_method = :git
+  # Optional Settings
+  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+   deploy.branch   = 'master' # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
